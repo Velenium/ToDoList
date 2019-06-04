3047494c8d54a453d774b978b9676e376e8dea69
Ursobaddude <lecoqq@ya.ru>
2019-06-04 10:09:52 +0300

    Update behaviour moved to own Validator

--- src/Task.php
+++ src/Task.php
@@ -7,28 +7,15 @@ use App\TaskData;
 
 class Task
 {
-	const MINLENGTH = 3;
-	const POSSIBLESTATUS = ['in progress', 'canceled', 'completed'];
-
-
 	private $taskData;
 
-	public function __construct(String $name, Uuid $id, String $body, String $status)
+	private function __construct(String $name, Uuid $id, String $body, String $status)
 	{
 		$this->taskData = new TaskData($name, $id, $body, $status);
 	}
 
-	public static function createNewTask(String $name, String $body)
+	public static function createNewTask(String $name, Uuid $id, String $body, String $status) : self
 	{
-		if (strlen($name) < self::MINLENGTH) {
-			return 'Minimum name length is ' . self::MINLENGTH;
-		}
-		elseif (strlen($body) < self::MINLENGTH) {
-			return 'Minimum body length is ' . self::MINLENGTH;
-		}
-
-		$id = Uuid::uuid4();
-		$status = 'new';
 		return new self($name, $id, $body, $status);
 	}
 
@@ -39,34 +26,11 @@ class Task
 
 	public function taskBodyUpdate(String $newBody)
 	{
-		if (strlen($newBody < self::MINLENGTH)) {
-			return 'Minimum body length is ' . self::MINLENGTH;
-		}
-		elseif ($this->taskData->status === 'completed') {
-			return 'Task already completed';
-		}
-		elseif ($this->taskData->status === 'canceled') {
-			return 'Task already canceled';
-		}
-
 		$this->taskData->body = $newBody;
 	}
 
 	public function taskStatusUpdate(String $newStatus)
 	{
-		if (!in_array($newStatus, self::POSSIBLESTATUS)) 
-		{
-			return 'Expected status: in progress/completed/canceled';
-		} 
-		elseif ($this->taskData->status === 'completed' && $newStatus === 'canceled') 
-		{
-			return 'Task already comleted';
-		} 
-		elseif ($this->taskData->status === 'canceled' && $newStatus === 'completed') 
-		{
-			return 'Task already canceled';
-		}
-
 		$this->taskData->status = $newStatus;
 	}
 
