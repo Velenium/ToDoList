49af9b88b2f45eda933a622e1f6bc06a8e3ac71b
Ursobaddude <lecoqq@ya.ru>
2019-06-04 10:22:58 +0300

    Interlayer between Controller/Service added

---
+++ src/ServiceResult.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App;
+
+class ServiceResult
+{
+	private $status;
+	private $body;
+
+	public function __construct()
+	{
+		$this->status = '200';
+	}
+
+	public function getBody()
+	{
+		return $this->body;
+	}
+
+	public function getStatus()
+	{
+		return $this->status;
+	}
+
+	public function setResponseBody($body)
+	{
+		$this->body = $body;
+	}
+
+	public function setResponseStatusError()
+	{
+		switch ($this->body) {
+			case 'Minimum name length is 3' : 
+				$this->status = '400';
+				break;
+			case 'Minimum body length is 3' :
+				$this->status = '400';
+				break;
+			case 'Task does not exist' :
+				$this->status = '404';
+				break;
+			case 'Expected status: in progress/completed/canceled' :
+				$this->status = '400';
+				break;
+			case 'Task already canceled' :
+				$this->status = '400';
+				break;
+			case 'Task already completed' :
+				$this->status = '400';
+				break;
+			case null :
+				$this->status = '204';
+		}
+	}
+}
