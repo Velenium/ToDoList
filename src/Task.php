79705a6ea90ec9d64a99671034e2d39ad4cd37b2
Ursobaddude <lecoqq@ya.ru>
2019-06-08 17:38:11 +0300

    Task updated

--- src/Task.php
+++ src/Task.php
@@ -4,38 +4,75 @@ namespace App;
 
 use Ramsey\Uuid\Uuid;
 use App\TaskData;
+use App\ResultConst;
 
 class Task
 {
-	private $taskData;
+	public const MINLENGTH = 3;
+	public const POSSIBLESTATUS = ['in progress', 'completed', 'canceled'];
 
-	private function __construct(String $name, Uuid $id, String $body, String $status)
+	private $errors;
+	private $name;
+	private $id;
+	private $body;
+	private $status;
+
+	private function __construct(
+		Array $errors,
+		String $name = null, Uuid $id  = null,
+		String $body = null, String $status = null)
 	{
-		$this->taskData = new TaskData($name, $id, $body, $status);
+		$this->errors = $errors;
+		$this->name = $name;
+		$this->id = $id;
+		$this->body = $body;
+		$this->status = $status;
 	}
 
-	public static function createNewTask(String $name, Uuid $id, String $body, String $status) : self
+	public static function createNewTask(
+		Array $errors,
+		String $name = null, Uuid $id  = null,
+		String $body = null, String $status = null) : self
 	{
-		return new self($name, $id, $body, $status);
-	}
-
-	public static function createFromDTO(TaskData $taskData) : self
-	{
-		return new self($taskData->name, $taskData->id, $taskData->body, $taskData->status);
+		return new self([], $name, $id, $body, $status);
 	}
 
 	public function taskBodyUpdate(String $newBody)
 	{
-		$this->taskData->body = $newBody;
+		if (strlen($newBody) < self::MINLENGTH) {
+			$this->errors = ResultConst::Task['302'];
+		} elseif ($this->status === 'completed') {
+			$this->errors = ResultConst::Task['303'];
+		} elseif ($this->status === 'canceled') {
+			$this->errors = ResultConst::Task['304'];
+		}
+
+		if (empty($this->errors)) {
+			$this->body = $newBody;
+		}
 	}
 
 	public function taskStatusUpdate(String $newStatus)
 	{
-		$this->taskData->status = $newStatus;
+		if (!in_array($newStatus, self::POSSIBLESTATUS)) 
+		{
+			$this->errors = ResultConst::Task['305'];
+		} 
+		if ($this->status === 'completed' && $newStatus === 'canceled') 
+		{
+			$this->errors = ResultConst::Task['303'];
+		} 
+		if ($this->status === 'canceled' && $newStatus === 'completed') 
+		{
+			$this->errors = ResultConst::Task['304'];
+		}
+		if (empty($this->errors)) {
+			$this->status = $newStatus;
+		}
 	}
 
 	public function getTaskData() : TaskData
 	{
-		return $this->taskData;
+		return new TaskData($this->errors, $this->name, $this->id, $this->body, $this->status);
 	}
 }
