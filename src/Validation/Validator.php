e98ed8f29dc77e74952f817c12f6e69615b80088
Ursobaddude <lecoqq@ya.ru>
2019-06-09 00:43:51 +0300

    Error const added

--- src/Validation/Validator.php
+++ src/Validation/Validator.php
@@ -2,22 +2,31 @@
 
 namespace App\Validation;
 
-use App\ResultConst;
-
 class Validator
 {
 	private $errors;
 
+	private const UUIDv4 = '/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i';
+
+	private const Error = [
+		'101' => ['error' => 'Invalid Name Given', 'code' => 400], 
+ 		'102' => ['error' => 'Invalid Body Given', 'code' => 400],
+ 		'103' => ['error' => 'Invalid Task ID Given', 'code' => 400],
+		'104' => ['error' => 'Invalid Param Given', 'code' => 400],
+		'105' => ['error' => 'Minimum name length is 3', 'code' => 411],
+		'106' => ['error' => 'Minimum body length is 3', 'code' => 411],
+	];
+
 	public function validateNewData($name, $body) : Array
 	{
 		if (!is_string($name)) {
-			$this->errors = ResultConst::Validator['101'];
+			$this->errors = self::Error['101'];
 		} elseif (strlen($name) < 3) {
-			$this->errors = ResultConst::Task['301'];
+			$this->errors = self::Error['105'];
 		} elseif (!is_string($body)) {
-			$this->errors = ResultConst::Validator['102'];
+			$this->errors = self::Error['102'];
 		} elseif (strlen($body) < 3) {
-			$this->errors = ResultConst::Task['302'];
+			$this->errors = self::Error['106'];
 		}
 		
 		return $this->errors === null ? [] : $this->errors;
@@ -25,14 +34,12 @@ class Validator
 
 	public function validateUpdateData($id, $newParam) : Array
 	{
-		$UUIDv4 = '/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i';
-
-		if (preg_match($UUIDv4, $id) !== 1)
+		if (preg_match(self::UUIDv4, $id) !== 1)
 		{
-			$this->errors = ResultConst::Validator['102'];
+			$this->errors = self::Error['102'];
 		}
 		if (!is_string($newParam)) {
-			$this->errors = ResultConst::Validator['104'];
+			$this->errors = self::Error['104'];
 		}
 
 		return $this->errors === null ? [] : $this->errors;
@@ -40,11 +47,9 @@ class Validator
 
 	public function validateID($id) : Array
 	{
-		$UUIDv4 = '/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i';
-
-		if (preg_match($UUIDv4, $id) !== 1)
+		if (preg_match(self::UUIDv4, $id) !== 1)
 		{
-			$this->errors = ResultConst::Validator['102'];
+			$this->errors = self::Error['102'];
 		}
 
 		return $this->errors === null ? [] : $this->errors;
